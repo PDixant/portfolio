48deb5a12dbf416a26737b3650e5bbddb3fc0b18
PDixant <dixant@uoguelph.ca>
2019-10-12 23:13:12 -0400

    start of portfolio

---
+++ myApp/my-profile/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import logo from './skyscrapper.jpg'
+function App() {
+  return (
+    <div>
+        <img src={logo} alt="skyscrapper"/>
+        <h1>
+            Hello World
+        </h1>
+    </div>
+  );
+}
+
+export default App;
