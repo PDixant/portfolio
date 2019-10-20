9b16549592a4750d819f860372b355a8e7063aa7
PDixant <dixant@uoguelph.ca>
2019-10-19 22:40:08 -0400

    change to -reactbootstrap maybe?

--- myApp/my-profile/src/App.js
+++ myApp/my-profile/src/App.js
@@ -2,17 +2,40 @@ import React from 'react';
 import './App.css';
 import logo from './skyscrapper.jpg'
 import styled from 'styled-components'
-const Krish = styled.p`
-font-size: 300px;
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+
+const LayoutBackground = styled.div`
+height: 400px;
+width: 100%;
+position: relative;
 `;
+
 function App() {
+
   return (
-    <div>
-        <img src={logo} width={'100%'} height={'400px'} alt="skyscrapper"/>
-        <Krish>
-          Hello, Krish
-        </Krish>
-    </div>
+    <LayoutBackground>
+    <Layout style={{background: `url(${logo}) center / cover`}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+    </LayoutBackground>
   );
 }
 
