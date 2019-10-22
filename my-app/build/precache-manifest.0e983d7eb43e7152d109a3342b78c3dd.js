1d8185fc811e5bb142d00243f2473dcc0762bc56
PDixant <dixant@uoguelph.ca>
2019-10-21 22:53:00 -0400

    Testing

---
+++ my-app/build/precache-manifest.0e983d7eb43e7152d109a3342b78c3dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78afdff135cddec00cc83205696cbdc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce29b282e079fdf52958",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "6d7629b9fbc96dcc7ba6",
+    "url": "/portfolio/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "ce29b282e079fdf52958",
+    "url": "/portfolio/static/js/2.f32ea3ef.chunk.js"
+  },
+  {
+    "revision": "6d7629b9fbc96dcc7ba6",
+    "url": "/portfolio/static/js/main.2e213353.chunk.js"
+  },
+  {
+    "revision": "b9df8e79685b67935d85",
+    "url": "/portfolio/static/js/runtime-main.f0586756.js"
+  },
+  {
+    "revision": "648b6ab2b8d87f267f413fd0ea7d58b9",
+    "url": "/portfolio/static/media/skyscrapper.648b6ab2.jpg"
+  }
+]);
