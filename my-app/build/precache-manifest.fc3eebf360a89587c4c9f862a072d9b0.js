2d801c26d2102c4bc9d4305a451d8b3bd96c4389
PDixant <dixant@uoguelph.ca>
2019-11-03 16:02:46 -0500

    update build

---
+++ my-app/build/precache-manifest.fc3eebf360a89587c4c9f862a072d9b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14df2a3c1ee695d3a3c291e502d89862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a7b225c8959b5bfb4e8",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "c8f3592aa4f9d12a5dac",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5a7b225c8959b5bfb4e8",
+    "url": "/static/js/2.87f966fe.chunk.js"
+  },
+  {
+    "revision": "c8f3592aa4f9d12a5dac",
+    "url": "/static/js/main.d7bf2cb3.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  },
+  {
+    "revision": "d60e2ec58f188dafec165fd6048b2d41",
+    "url": "/static/media/profile.d60e2ec5.png"
+  }
+]);
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('root')
 );
-serviceWorker.unregister();
+//serviceWorker.unregister();
 
 // import React from 'react';
 // // import ReactDOM from 'react-dom';
