b8b9cef1d642cde86759b92a5368aed700802a3f
PDixant <dixant@uoguelph.ca>
2020-02-08 18:22:02 -0500

    Updates

--- precache-manifest.8303c12e6f82ffb69be32f5c595819a1.js
+++ precache-manifest.8303c12e6f82ffb69be32f5c595819a1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8eb10159e76ec1c25322a03db207ea88",
+    "revision": "f9f558956d6d7c666bb221ffcba133ed",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "9d39ea1fb65f6397b84a",
+    "revision": "88a7d24fd8a193131b0b",
     "url": "/portfolio/static/css/2.559fd73f.chunk.css"
   },
   {
-    "revision": "c6aa07818bccabb86b14",
+    "revision": "975990ba2c77c394da84",
     "url": "/portfolio/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "9d39ea1fb65f6397b84a",
-    "url": "/portfolio/static/js/2.d846bdbb.chunk.js"
+    "revision": "88a7d24fd8a193131b0b",
+    "url": "/portfolio/static/js/2.815b69a8.chunk.js"
   },
   {
     "revision": "e4f29d3e48fc4028c83e9a3472164e2b",
-    "url": "/portfolio/static/js/2.d846bdbb.chunk.js.LICENSE"
+    "url": "/portfolio/static/js/2.815b69a8.chunk.js.LICENSE"
   },
   {
-    "revision": "c6aa07818bccabb86b14",
-    "url": "/portfolio/static/js/main.ecf1a9e5.chunk.js"
+    "revision": "975990ba2c77c394da84",
+    "url": "/portfolio/static/js/main.e2cbb3ae.chunk.js"
   },
   {
     "revision": "3372145aa467f5e49196",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ec99e174e82c4f5dad759ef91d586b40.js"
+  "/portfolio/precache-manifest.8303c12e6f82ffb69be32f5c595819a1.js"
 );
 
 self.addEventListener('message', (event) => {
