05c02451464dcc2e676d400e7b3399ddeed44bf7
PDixant <dixant@uoguelph.ca>
2020-11-03 21:16:03 -0500

    Updates

---
+++ precache-manifest.bec4466651ff841ec58e7e605b60a2ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba6a78a4a704e098a05b35792903ae8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f59aede72dbf766a95fe",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "0bf0dcb0248f0e989ed6",
+    "url": "/portfolio/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f59aede72dbf766a95fe",
+    "url": "/portfolio/static/js/2.1cb6b72c.chunk.js"
+  },
+  {
+    "revision": "dcc96ab0bcc49d72e3ca12b1d3c14c9d",
+    "url": "/portfolio/static/js/2.1cb6b72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf0dcb0248f0e989ed6",
+    "url": "/portfolio/static/js/main.641f1c7a.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "940f3886c4bb692b253fa544f1885516",
+    "url": "/portfolio/static/media/profile.940f3886.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8303c12e6f82ffb69be32f5c595819a1.js"
+  "/portfolio/precache-manifest.bec4466651ff841ec58e7e605b60a2ff.js"
 );
 
 self.addEventListener('message', (event) => {
