b89eca7980a84d135393248341627e3eea586dd0
PDixant <dixant@uoguelph.ca>
2020-11-10 18:36:11 -0500

    build update

---
+++ my-app/build/precache-manifest.df3edd538064092d22ff8fac4b918d5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e3ce7090584b85c3e11595646c502b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c27df227184a43e478e2",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "be48999d4a6c8ce7f64f",
+    "url": "/portfolio/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c27df227184a43e478e2",
+    "url": "/portfolio/static/js/2.108e2688.chunk.js"
+  },
+  {
+    "revision": "dcc96ab0bcc49d72e3ca12b1d3c14c9d",
+    "url": "/portfolio/static/js/2.108e2688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be48999d4a6c8ce7f64f",
+    "url": "/portfolio/static/js/main.0ad456fd.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "940f3886c4bb692b253fa544f1885516",
+    "url": "/portfolio/static/media/profile.940f3886.jpg"
+  }
+]);
