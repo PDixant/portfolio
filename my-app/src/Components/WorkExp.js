27403655ba33225f7e00aae58e6ed507aa29843a
PDixant <dixant@uoguelph.ca>
2021-02-26 23:18:41 -0500

    added exp and disc

--- my-app/src/Components/WorkExp.js
+++ my-app/src/Components/WorkExp.js
@@ -29,17 +29,38 @@ font-size: ${props => (props.ssize > 1250) ? '50px' : '20px'}
 padding-bottom: 30px;
 `;
 
-const CompDisc = styled.div`
+const CompDiscContainer = styled.div`
 text-align: left;
 float: right;
 background: #FFC857;
 display: inline-block;
 width: 40%;
 overflow: hidden;
-font-size: ${props => (props.ssize > 1250) ? '30px' : '12px'}
 padding: 20px;
 color: #3b3838;
 `;
+
+const CompDisc = styled.span`
+font-size: ${props => (props.ssize > 1250) ? '20px' : '12px'}
+padding-top: 15px;
+padding-bottom: 15px;
+`;
+
+const RoleTitleContainer = styled.div`
+text-align: left;
+display: inline-block;
+overflow: hidden;
+width: 40%;
+background: #FFC857;
+font-weight: bold;
+font-size: ${props => (props.ssize > 1250) ? '50px' : '20px'}
+padding: 30px;
+`;
+
+const RoleDisc = styled.span`
+font-size: ${props => (props.ssize > 1250) ? '20px' : '12px'}
+`;
+
 class WorkExp extends Component{
     constructor(props){
         super(props);
@@ -64,24 +85,52 @@ class WorkExp extends Component{
                 Canvass Analytics
             </CompName>
             </Cell>
+            <br/>
+            <br/>
             <Cell col={12}>
-            <CompDisc>
+            <CompDiscContainer ssize={screenSize}>
             <CompDiscTitle ssize={screenSize}>
                 What is Canvass?
             </CompDiscTitle>
                 <br/>
                 <br/>
-                Canvass is an industrial IoT based platform solution
-                that is a global leader in industrial AI located at the
-                heart of downtown Toronto. The platform accelerates
-                mission-critical decision making to minimize energy
-                consumption, Increase throughput, reduce downtime and
-                extend the lifetime of a plant’s assets. Canvass has
-                been funded by Google’s Gradient venture funds and
-                Fortune 500 companies use the platform to optimize
-                complex production processes, generate new revenue
-                streams, and reduce energy and operating costs.
-            </CompDisc>
+                <CompDisc ssize={screenSize}>
+                    Canvass is an industrial IoT based platform solution
+                    that is a global leader in industrial AI located at the
+                    heart of downtown Toronto. The platform accelerates
+                    mission-critical decision making to minimize energy
+                    consumption, Increase throughput, reduce downtime and
+                    extend the lifetime of a plant’s assets. Canvass has
+                    been funded by Google’s Gradient venture funds and
+                    Fortune 500 companies use the platform to optimize
+                    complex production processes, generate new revenue
+                    streams, and reduce energy and operating costs.
+                </CompDisc>
+            </CompDiscContainer>
+            </Cell>
+            <Cell col={12}>
+            <RoleTitleContainer ssize={screenSize}> 
+                What is my role ?
+                <br/>
+                <br/>
+            <RoleDisc ssize={screenSize}>
+                My job has as a Software Engineer intern was to work
+                on the front end of the Canvass AI/ML platform, performing
+                tasks like optimizing user experience. The position consisted
+                of building new components and containers for users to be
+                able to new features of the platform, participating in Scrum
+                and taking on demo’s. As an intern, being able to
+                participating in design meetings, team demo’s, socials and
+                scrum as a whole has provided me with the confidence and
+                powered my passion even more to pursue this amazing field
+                of software. There were many needed skills for this position,
+                such as the knowledge in ReactJS and JavaScript, and soft
+                skills including communications, decision making, time
+                management, Adaptability and the Ability to work under
+                pressure. I was lucky to be able to expand on my skills
+                on the job as a part of the team.
+            </RoleDisc>
+            </RoleTitleContainer>
             </Cell>
         </Grid>
         </div>
