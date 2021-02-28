e40ea4684cb32e7ebf5da58322443155ca829b39
PDixant <dixant@uoguelph.ca>
2021-02-27 19:06:12 -0500

    update text

--- my-app/src/Components/WorkExp.js
+++ my-app/src/Components/WorkExp.js
@@ -131,20 +131,20 @@ class WorkExp extends Component{
                         My job as a Software Engineer intern was to work
                         on the front end of the Canvass AI/ML platform, performing
                         tasks like optimizing user experience, reducing time and effort it
-                        it would take the user to complete a task and add game changing algorithm
+                        would take the user to complete a task and add game changing algorithm
                         to increase speed and usability of the platform. The position consisted
-                        of building new components and containers for users to be
-                        able to use new features of the platform, participating in Scrum
+                        of building new components and bridge the connection of client and server
+                        for features implementations, participating in Scrum
                         and taking on demo’s. As an intern, being able to
                         participating in design meetings, team demo’s, socials and
                         scrum as a whole has provided me with the confidence and
                         powered my passion even more to pursue this amazing field
                         of software. There were many needed skills for this position,
-                        such as the knowledge in ReactJS and JavaScript, and soft
-                        skills including communications, decision making, time
+                        such as the knowledge in ReactJS and Redux, RxJS and other frameworks/ libraries,
+                        and soft skills including communications, decision making, time
                         management, Adaptability and the Ability to work under
                         pressure. I was lucky to be able to expand on my skills
-                        on the job as a part of the team.
+                        on the job as a part of the team be be able grow my personality thanks to my team members.
                     </RoleDisc>
                     </RoleTitleContainer>
                     </Cell>
